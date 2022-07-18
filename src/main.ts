99efc3abc2ace75364e6255e4f42268ebe0a275f
WheatleyCODE <WheatleyCODE@users.noreply.github.com>
2022-07-18 15:05:32 +0300

    Create swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
@@ -8,6 +9,17 @@ async function start() {
     const app = await NestFactory.create(AppModule);
     app.use(cookieParser());
     app.enableCors({ credentials: true, origin: process.env.URL_CLIENT });
+
+    const config = new DocumentBuilder()
+      .setTitle('Gamify Project')
+      .setDescription('Project for leveling skills')
+      .setVersion('1.0.0')
+      .addTag('Gamify')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('/api/docs', app, document);
+
     await app.listen(PORT, () => console.log('Server started on PORT:', PORT));
   } catch (e) {
     console.log(e);
